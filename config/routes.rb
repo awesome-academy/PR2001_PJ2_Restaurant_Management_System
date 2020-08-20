a164675b38e7571be9ec85f589eaa2d26a8bede8
Puck <zpuck97@gmail.com>
2020-08-20 11:49:58 +0700

    gem

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+   as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  root 'pages#home'
+  get 'home', to: 'pages#home'
+end
